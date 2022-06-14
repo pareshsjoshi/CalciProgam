3337344ce1ce77eafe12c0a387d57d01dd2f9df7
PareshSJoshi <pareshjoshi234@gmail.com>
2022-06-14 11:01:25 +0530

    GG

---
+++ CalciProgram.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main4() {
+
+    char op;
+    double first, second;
+    printf("Enter an operator (+, -, *, /): ");
+    scanf_s("%c", &op);
+    printf("Enter two operands: ");
+    scanf_s("%lf %lf", &first, &second);
+
+    switch (op) {
+    case '+':
+        printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+        break;
+    case '-':
+        printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+        break;
+    case '*':
+        printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+        break;
+    case '/':
+        printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+        break;
+        // operator doesn't match any case constant
+    default:
+        printf("Error! operator is not correct");
+    }
+
+    return 0;
+}
